2f83db67a154f309d96ca2a9f799eeafdd29757c
Kyle <kjulian3@stanford.edu>
2019-08-05 23:25:57 +0000

    Clean notebook, add networks

--- PolicyViz/PolicyViz.jl/src/PolicyViz.jl
+++ PolicyViz/PolicyViz.jl/src/PolicyViz.jl
@@ -5,6 +5,5 @@ using GridInterpolations, Interact, PGFPlots, Colors, ColorBrewer, HDF5, SparseA
 include("./viz_policy_constants.jl")
 include("./viz_policy.jl")
 include("./nnet_calculations.jl")
-include("./policy_functions.jl")
 
 end
